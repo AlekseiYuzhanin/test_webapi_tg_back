de69ca8df448dd3a3a46d3b30a67a97bf7b72384
alexey <aleksei.yuiuzhanin@yandex.ru>
2024-06-13 01:30:33 +0700

    conn

--- main.js
+++ main.js
@@ -12,14 +12,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-bot.on('message',async (msg) => {
-    const text = msg.text;
+bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
+    const text = msg.text;
+
     if(text === '/start') {
         await bot.sendMessage(chatId, 'Ниже появится кнопка, заполни форму', {
             reply_markup: {
                 keyboard: [
-                    [{text: 'Заполнить форму', web_app: {url: WebAppUrl + '/form'}}]
+                    [{text: 'Заполнить форму', web_app: {url: webAppUrl + '/form'}}]
                 ]
             }
         })
@@ -27,45 +28,46 @@ bot.on('message',async (msg) => {
         await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
                 inline_keyboard: [
-                    [{text: 'Сделать заказ', web_app: {url: WebAppUrl}}]
+                    [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
                 ]
             }
         })
     }
 
-    if (msg?.web_app_data?.data){
-        try{
+    if(msg?.web_app_data?.data) {
+        try {
             const data = JSON.parse(msg?.web_app_data?.data)
+            console.log(data)
+            await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
+            await bot.sendMessage(chatId, 'Ваша страна: ' + data?.country);
+            await bot.sendMessage(chatId, 'Ваша улица: ' + data?.street);
 
-           await bot.sendMessage(chatId, 'Спасибо за обратную связь!')
-            await bot.sendMessage(chatId, 'Ваша страна ' + data.country)
-            await bot.sendMessage(chatId, 'Ваша улица ' + data.street)
-            await bot.sendMessage(chatId, 'Вы блять... ' + data.subject)
-        }catch(err){
-
+            setTimeout(async () => {
+                await bot.sendMessage(chatId, 'Всю информацию вы получите в этом чате');
+            }, 3000)
+        } catch (e) {
+            console.log(e);
         }
     }
 });
 
 app.post('/web-data', async (req, res) => {
-    const {queryId, products, totalPrice, } = res.body;
-    try{
+    const {queryId, products = [], totalPrice} = req.body;
+    try {
         await bot.answerWebAppQuery(queryId, {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
             input_message_content: {
-                message_text : `Поздравляю вы приобрели товары на сумму ${totalPrice}`
+                message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
             }
         })
-
         return res.status(200).json({});
-    }catch(err){
-        console.log(err)
-        return res.status(500).json({});
+    } catch (e) {
+        return res.status(500).json({})
     }
-
 })
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
